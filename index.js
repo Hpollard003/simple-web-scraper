cfdb13f4a724d41c461aad921fa0c5b9b418ffed
Hpollard003 <hpollard003@gmail.com>
2022-04-20 02:20:36 -0400

    Add routes

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const axios = require('axios');
-const express = require('express');
-const cheerio = require('cheerio');
-const PORT = 8000
+const axios = require("axios");
+const express = require("express");
+const cheerio = require("cheerio");
+const PORT = 8000;
 
-const app = express()
-axios("https://www.theguardian.com")
-    .then(resp => {
-        const html = resp.data
-        const $ = cheerio.load(html)
-        const articles = []
+const app = express();
 
-        $('.fc-item__title', html).each(function() {
-            const title = $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({ title, url })
-        })
-        console.log(articles)
-    }).catch(err => console.log(err))
+app.get('/', function (req, res) {
+    res.json('Simple Web Scraper');
+})
 
+app.get("/results", (req, res) => {
+  axios("https://www.theguardian.com")
+    .then((resp) => {
+      const html = resp.data;
+      const $ = cheerio.load(html);
+      const articles = [];
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+      $(".fc-item__title", html).each(function () {
+        const title = $(this).text();
+        const url = $(this).find("a").attr("href");
+        articles.push({ title, url });
+      });
+      res.json(articles);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
